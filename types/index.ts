362b444398d275ef8966faad607f378e44e9f907
Tobibor Rahman <r.tobibor16@gmail.com>
2024-02-05 00:37:28 +0600

    search box created with headless ui

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
+import { manufacturers } from './../constants/index';
 import { MouseEventHandler } from 'react';
 
 export interface CustomButtonProps {
@@ -5,4 +6,9 @@ export interface CustomButtonProps {
 	type?: 'button' | 'submit';
 	containerStyles?: string;
 	handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface searchManufacturerProps {
+	manufacturer: string;
+	setManufacturer: (manufacturer: string) => void;
 }
