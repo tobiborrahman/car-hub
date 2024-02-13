a788c6e25af9ef419666953b2c455094de1407a2
Tobibor Rahman <r.tobibor16@gmail.com>
2024-02-13 13:37:56 +0600

    car updated

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,22 +1,36 @@
 'use client';
 
 import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
 import React from 'react';
 
 const CustomButton = ({
 	title,
 	containerStyles,
 	handleClick,
-	type,
+	btnType,
+	textStyles,
+	rightIcon,
 }: CustomButtonProps) => {
 	return (
 		<button
 			disabled={false}
-			type={type || 'button'}
+			type={btnType || 'button'}
 			className={`custom-btn ${containerStyles}`}
 			onClick={handleClick}
 		>
-			<span className="flex-1">{title}</span>
+			<span className={`flex-1 ${textStyles}`}>{title}</span>
+
+			{rightIcon && (
+				<div className="relative w-6 h-6">
+					<Image
+						src={rightIcon}
+						alt="right icon"
+						fill
+						className="object-contain"
+					/>
+				</div>
+			)}
 		</button>
 	);
 };
--- types/index.ts
+++ types/index.ts
@@ -3,12 +3,30 @@ import { MouseEventHandler } from 'react';
 
 export interface CustomButtonProps {
 	title: string;
-	type?: 'button' | 'submit';
+	btnType?: 'button' | 'submit';
 	containerStyles?: string;
 	handleClick?: MouseEventHandler<HTMLButtonElement>;
+	textStyles?: string;
+	rightIcon?: string;
+	isDisabled?: boolean;
 }
 
 export interface searchManufacturerProps {
 	manufacturer: string;
 	setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+	city_mpg: number;
+	class: string;
+	combination_mpg: number;
+	cylinders: number;
+	displacement: number;
+	drive: string;
+	fuel_type: string;
+	highway_mpg: number;
+	make: string;
+	model: string;
+	transmission: string;
+	year: number;
 }
