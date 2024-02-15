e270b11e9f3ab5b7fd1401e0a47cc07b539a4bb8
Tobibor Rahman <r.tobibor16@gmail.com>
2024-02-16 02:11:55 +0600

    done

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -6,13 +6,10 @@ import React from 'react';
 import { CustomButton } from '.';
 import { updateSearchParams } from '@/utils';
 
-const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
-	const router = useRouter();
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
 	const handleNavigation = () => {
 		const newLimit = (pageNumber + 1) * 10;
-		const newPathName = updateSearchParams('limit', `${newLimit}`);
-
-		router.push(newPathName);
+		setLimit(newLimit);
 	};
 	return (
 		<div className="w-full flex-center gap-5 mt-10">
